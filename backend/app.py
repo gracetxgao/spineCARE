feb39968580f58271a06cb2b4b39a62d9f579bfd
gracetxgao <101677420+gracetxgao@users.noreply.github.com>
2024-06-22 16:55:09 -0700

    edit cors permissions again

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ from PIL import Image
 import io
 
 app = Flask(__name__)
-CORS(app, origins=['http://localhost:3000', 'https://spinecare.vercel.app'])
+CORS(app, origins=['http://localhost:3000', 'https://spinecare.vercel.app/', 'https://spinecare-graces-projects-2051e3b2.vercel.app/'])
 
 model, preprocess = create_model_from_pretrained('hf-hub:microsoft/BiomedCLIP-PubMedBERT_256-vit_base_patch16_224')
 tokenizer = get_tokenizer('hf-hub:microsoft/BiomedCLIP-PubMedBERT_256-vit_base_patch16_224')
